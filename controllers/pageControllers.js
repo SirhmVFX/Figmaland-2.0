0fb8459a82a8ab1871cf7a2d922b92a12a5683b5
sirhmvfx <sirhmvfx@gmail.com>
2023-03-08 11:05:38 +0100

    builidng second page

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,3 +1,7 @@
 exports.homepage = function(req, res) {
-    res.render("homepage")
+    res.render("homepage", {pageTitle: "homepage"})
+}
+
+exports.productPage = function(req, res) {
+    res.render("product", {pageTitle: "Products"})
 }
--- router.js
+++ router.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const pageControllers = require("./controllers/pageControllers")
 
 router.get("/", pageControllers.homepage)
+router.get("/product", pageControllers.productPage)
 
 module.exports = router
